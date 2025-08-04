486ee2fd554e2730850ae9a4d5f3c7c7025075a5
Himanshu Choudhary <himanshukakran8@gmail.com>
2025-08-05 00:56:14 +0530

    fixing backend issue

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
 import geminiRoute from "./routes/gemini.route.js";
 import chatbotRoute from "./routes/chat.route.js";
 import paymentRoute from "./routes/stripe.route.js";
@@ -10,9 +8,7 @@ import Webhookstripe from './routes/stripe.webhook.route.js';
 
 dotenv.config();
 
-// Fix for __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 
 const app = express();
 
@@ -33,12 +29,6 @@ app.use("/api/payment", paymentRoute);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
-});
-
-app.use(express.static(path.join(__dirname, "client/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/dist", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
