89584e2d0cbd60ed54eac8cbc8cb7d77b38be474
Himanshu Choudhary <himanshukakran8@gmail.com>
2025-08-04 18:33:31 +0530

    update firebase config

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,6 +4,8 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getDocs } from "firebase/firestore";
 
+import { getAnalytics } from "firebase/analytics";
+
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -11,13 +13,15 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);  // Firestore DB
 export const auth = getAuth(app);     // Auth
+const analytics = getAnalytics(app);
 
 export default app;
 
