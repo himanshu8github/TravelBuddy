26b3f6f9ff81c5ce7e7e16006ee5c21a450366a1
Himanshu Choudhary <himanshukakran8@gmail.com>
2025-08-05 00:12:57 +0530

    update .env

--- server/server.js
+++ server/server.js
@@ -33,6 +33,14 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/dist", "index.html"));
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(` Server running on http://localhost:${PORT}`);
